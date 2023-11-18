c19a9721097c8bae619dc0cdf3d50a5563991448
sinjed <oooguuh@gmail.com>
2023-11-18 20:44:35 +0100

    Remove starting start file after install
    Signed-off-by: sinjed <oooguuh@gmail.com>

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import os
 
 # Windows
 if os.name == 'nt':
-    cmd = 'python3 -m venv .venv && echo ".venv\Scripts\\activate && start.bat" > run.bat && start.bat && pip install -r requirements.txt'
+    cmd = 'python3 -m venv .venv && echo ".venv\Scripts\\activate && start.bat" > run.bat && start.bat'
     os.system(cmd)
 else:
-    cmd = 'python3 -m venv .venv && echo ".venv/bin/activate && ./start" > run.sh && chmod +x start  && ./start && pip3 install -r requirements.txt'
+    cmd = 'python3 -m venv .venv && echo ".venv/bin/activate && ./start" > run.sh && chmod +x start  && ./start'
     os.system(cmd)    
