4c67cd4a13247a81dafca0c1a1ad2a18866b4ccf
sinjed <oooguuh@gmail.com>
2023-11-18 20:46:25 +0100

    Fixed filename
    Signed-off-by: sinjed <oooguuh@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 # Windows
 if os.name == 'nt':
-    cmd = 'python3 -m venv .venv && echo ".venv\Scripts\\activate && start.bat" > run.bat && start.bat'
+    cmd = 'python3 -m venv .venv && echo ".venv\Scripts\\activate && start.bat" > start.bat && start.bat'
     os.system(cmd)
 else:
     cmd = 'python3 -m venv .venv && echo ".venv/bin/activate && ./start" > run.sh && chmod +x start  && ./start'
