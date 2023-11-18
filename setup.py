d6410be09e2689076294228926aff7677b6c1de5
sinjed <oooguuh@gmail.com>
2023-11-18 21:10:51 +0100

    Removed setup.py
    Signed-off-by: sinjed <oooguuh@gmail.com>

--- setup.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Windows
-if os.name == 'nt':
-    cmd = 'python3 -m venv .venv && echo ".venv\Scripts\\activate && start.bat" > start.bat && start.bat'
-    os.system(cmd)
-else:
-    cmd = 'python3 -m venv .venv && echo ".venv/bin/activate && ./start" > start.sh && chmod +x start  && ./start'
-    os.system(cmd)    
